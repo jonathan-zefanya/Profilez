81cbef1ea00cadf475fcc959fd07c1e21c453f9b
jonathan-zefanya <nakano10909@gmail.com>
2025-12-10 16:10:43 +0700

    first commit

---
+++ vendor/kreativdev/saas-installer/src/config/kdinstaller.php
@@ -0,0 +1,12 @@
+<?php 
+
+    return [
+        'item_name' => "Profilex",
+        "item_id" => 33283445,
+        "vension" => 4.0,
+        "released_on" => "8th May, 2025",
+        "broadcast_driver" => "log",
+        "email_api" => 'https://kreativdev.com/emailcollector/api/collect'
+    ];
+
+?>
